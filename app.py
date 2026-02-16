a5c4702312c05d9a27c75120cda84cf25b6c1655
Madr50 <lyf67490@gmail.com>
2026-02-16 19:15:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+import pyfiglet
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def show_love():
+    # اختيار نوع الخط المزخرف (هنا استخدمنا 'slant' ويمكنك تجربة 'block')
+    big_text = pyfiglet.figlet_format("I LOVE YOU", font="slant")
+    print(big_text)
+    
+    # إضافة لمسة جمالية بسيطة باللغة العربية
+    print("      ♥ أحبـــــك ♥      ")
+
+if __name__ == "__main__":
+    show_love()
